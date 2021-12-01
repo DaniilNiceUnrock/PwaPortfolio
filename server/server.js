17eac50e196fef710bec262e43b31e88c76dcd68
DaniilNiceUnrock <84072104+DaniilNiceUnrock@users.noreply.github.com>
2021-12-01 14:00:45 +0300

    DATABASE
    PWA

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.listen(app.get('port'), () => {
 
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://Daniil:090974papa@cluster0.wbyey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Daniil:090974papa@cluster0.wbyey.mongodb.net/PWA?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
