6d857215fa0574210f477b7122448ef48bb23708
DaniilNiceUnrock <84072104+DaniilNiceUnrock@users.noreply.github.com>
2021-12-01 14:43:58 +0300

    Update author.js

--- server/models/author.js
+++ server/models/author.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-const Author = new Schema(
-  {
+const Author = new Schema({
     first_name: {type: String, required: true, max: 100},
     family_name: {type: String, required: true, max: 100},
     date_of_birth: {type: Date},
     date_of_death: {type: Date},
-  }
-);
+});
 
 
-//Export model
-module.exports = mongoose.model('Author', Author);
-
+module.exports = mongoose.model('author', Author);
