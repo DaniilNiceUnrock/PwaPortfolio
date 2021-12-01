81de95896ca0efa519550377764ee03310b3717b
DaniilNiceUnrock <84072104+DaniilNiceUnrock@users.noreply.github.com>
2021-12-01 12:45:29 +0300

    Update Skills.js
    skills

--- server/models/Skills.js
+++ server/models/Skills.js
@@ -2,13 +2,47 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const Skills = new Schema({
-    
-    Skill: String,
-    Skills:   {
-        name : String,
-        precent: Number
-    }
+    category: String,
+    skills: String  [
+        {   
+            id: Number,
+            name : String,
+            precent: Number
+        }
+    ]
 });
+
+
+/*
+[
+    {
+      "id": 1,
+      "category": "Frontend",
+      "skills": [
+        {
+          "id": 1,
+          "title": "HTML",
+          "percent": 90
+        },
+        {
+          "id": 2,
+          "title": "CSS",
+          "percent": 76
+        },
+        {
+          "id": 3,
+          "title": "JavaScript",
+          "percent": 25
+        },
+        {
+          "id": 4,
+          "title": "Vue.js",
+          "percent": 35
+        }
+      ]
+    },
+  ]
+*/
 
 /* db.Skills.insertOne({"name": "Test", "precent": 28}) */
 
