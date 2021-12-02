130b5dab19f6040e52a620c23aaec0f527a2de49
DaniilNiceUnrock <84072104+DaniilNiceUnrock@users.noreply.github.com>
2021-12-02 09:10:58 +0300

    Update Rewiews.js

--- server/models/Rewiews.js
+++ server/models/Rewiews.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-
 const Rewiews = new Schema({
     name: String,
     body: String,
-    date: Date
+    publishedAt:{ 
+        type:Date,             
+        default:Date.now()      
+    }
 });
 
 module.exports = mongoose.model('Rewiews', Rewiews);
